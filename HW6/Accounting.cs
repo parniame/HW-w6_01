34316af3ee2b8552d44078ff3453bd04bae00311
parniame <pzedbaz@gmail.com>
2024-06-13 17:04:46 +0330

    First commit

---
+++ HW6/Accounting.cs
@@ -0,0 +1,82 @@
+﻿namespace HW6
+{
+    public class Accounting
+    {
+        public int hourCountMonthly {
+            get { return hourCountMonthly; }
+
+
+
+            set
+            {
+
+
+                if ((0 <= value) && (value <= 300))
+                {
+                    hourCountMonthly = value;
+                }
+
+            }
+        }
+        public float IncomePerHour
+        {
+            get { return IncomePerHour; }
+
+
+
+            set
+            {
+                
+
+                if ((0 <= value))
+                {
+                    IncomePerHour = value;
+                }
+
+            }
+
+
+        }
+        public float Tax
+        {
+            get { return Tax; }
+
+
+
+            set
+            {
+                
+                
+                if ( (0 <= value) && (value  < 1))
+                {
+                    Tax = value;
+                }
+                
+            }
+
+
+        }
+        public Accounting()
+        {
+            Tax = .1f;
+            
+        }
+        public  virtual float TaxCalculator(float amount)
+        {
+            return amount*Tax;
+        }
+        public virtual float IncomeCalculator()
+        {
+            return hourCountMonthly*IncomePerHour;
+        }
+
+    }
+    public class Gold
+    {
+
+    }
+    public class GoldSale : Accounting
+    {
+
+    }
+}
---
+++ HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
